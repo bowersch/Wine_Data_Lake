779e0daa702a0f865e55e00acbf6c4897d867309
Austin Chayka <chaykaa@oregonstate.edu>
2023-01-05 14:27:31 -0800

    Test Server
    Uploaded test server files as well as updated the .gitignore to not upload npm dependencies

---
+++ test_server/readme.txt
@@ -0,0 +1,8 @@
+Setup:
+    npm init
+    ^downloads required node packages
+Before Useage:
+    update config.json with correct template and data files
+Run: 
+    npm start
+    ^ command will run/update server continuously, so there is no need to rerun in between file updates
---
+++ test_server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const config = require('./config.json');
+const test_data = require('./' + config.test_data);
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebars.engine({
+    layoutsDir : config.layouts_path
+}));
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render(config.template, {layout : config.layout_template, data : test_data});
+});
+
+app.listen(port, () => console.log(`App open at http://localhost:${port}/`));
