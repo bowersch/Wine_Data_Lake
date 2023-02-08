1a3cfe9cdcd065c6017a2b22a3ff5313aef8a7c5
Austin Chayka <chaykaa@oregonstate.edu>
2023-02-08 15:37:07 -0800

    Port Change

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const testData = require('./demo.json')
 
