43783d6bdeb6e3ffe1497c77deec58245d722060
Austin Chayka <chaykaa@oregonstate.edu>
2023-02-15 18:04:06 -0800

    Query test
    query added to test db data

--- server/main.js
+++ server/main.js
@@ -3,6 +3,28 @@ const handlebars = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const { Client } = require("pg");
+const secrets = require("./secrets.json")
+
+const client = new Client({
+    host: '34.168.133.161',
+    port: 5432,
+    user: secrets.user,
+    password: secrets.password
+});
+client.connect((err) => {
+    if(err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('connected')
+    }
+});
+client.query('SELECT wine_name FROM bottle_data;', (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    client.end();
+});
+
 const testData = require('./demo.json')
 
 app.set('view engine', 'handlebars');
@@ -12,62 +34,6 @@ app.engine('handlebars', handlebars.engine({
 }));
 
 app.use(express.static('public'))
-
-app.get('/wines/*', (req, res) => {
-    res.render('wineEntry', {
-        layout : 'main',
-        css: ["wineEntry.css"],
-        data: testData
-    });
-});
-
-app.get('/directory/*', (req, res) => {
-    res.render('directory', {
-        layout: 'main',
-        css: ["directory.css"],
-        data: testData
-    });
-});
-
-app.get('/favorites/*', (req, res) => {
-    res.render('userFavorites', {
-        layout: 'main',
-        css: ["userFavorites.css"],
-        data: testData
-    });
-});
-
-app.get('/about*', (req, res) => {
-    res.render('about', {
-        layout: 'main',
-        css: ["about.css"],
-        data: testData
-    });
-});
-
-app.get('/contact*', (req, res) => {
-    res.render('contact', {
-        layout: 'main',
-        css: ["about.css"],
-        data: testData
-    });
-});
-
-app.get('/developers*', (req, res) => {
-    res.render('developers', {
-        layout: 'main',
-        css: ["about.css"],
-        data: testData
-    });
-});
-
-app.get('/help*', (req, res) => {
-    res.render('help', {
-        layout: 'main',
-        css: ["about.css"],
-        data: testData
-    });
-});
 
 app.get('/*', (req, res) => {
     res.render('demo', {layout : 'main', css: ["demo.css"]});
