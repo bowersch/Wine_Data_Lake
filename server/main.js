0948701a2559bc76fccd926a08e13d4d8984ed9c
Austin Chayka <chaykaa@oregonstate.edu>
2023-03-19 18:59:28 -0700

    connection

--- server/main.js
+++ server/main.js
@@ -18,7 +18,7 @@ var user = secrets.getSecret('DB_USER');
 var pass = secrets.getSecret('DB_PASS');
 
 const conn = new Pool({
-    host: '10.115.240.3',
+    host: 'winedatalake-377223:us-west1:wdl-postgre',
     port: 5432,
     database: "postgres",
     user: user,
