992db676eb76e636b6953f1ea2baa63cc444a746
CWH2000 <huangc5@oregonstate.edu>
2023-04-30 12:45:13 -0700

    fetch from main

---
+++ queryHelper.js
@@ -0,0 +1,189 @@
+exports.getWineriesWines = (client, callback) => {
+    var q = "SELECT wineries.winery_id, wineries.winery_name FROM wineries ORDER BY winery_name ASC;";
+    client.query(q, (err, res) => {
+        if (err) {
+            console.log(err);
+            callback(null);
+        }
+        else {
+            var wineries = [];
+            for (let i = 0; i < res.rows.length; i++) {
+                client.query("SELECT bottle_data.bottle_id, bottle_data.year, bottle_data.wine_name FROM bottle_data WHERE bottle_data.winery_id = $1;",
+                    [res.rows[i].winery_id],
+                    (err2, res2) => {
+                        if (err2) console.log(err2);
+                        let x = {
+                            "winery_name" : res.rows[i].winery_name,
+                            "wines": res2.rows
+                        };
+                        wineries[i] = x;
+                        if(i == res.rows.length - 1) {
+                            callback(wineries);
+                        }
+                    }
+                );
+            }
+        }
+    });
+}
+
+exports.getWineInfo = (id, client, callback) => {
+    var q = "SELECT bottle_data.wine_name, bottle_data.pct_alcohol, bottle_data.ta, bottle_data.ph, bottle_data.year, techsheets.source_file, wineries.winery_name FROM bottle_data INNER JOIN wineries ON bottle_data.winery_id = wineries.winery_id INNER JOIN techsheets ON bottle_data.techsheet_id = techsheets.techsheet_id WHERE bottle_id = $1;";
+    client.query(q, [id], (err, res) => {
+        if (err) {
+            console.log(err);
+            callback(null);
+        } else {
+            callback({
+                "name" : res.rows[0].wine_name,
+                "keywords" : [res.rows[0].year],
+                "description" : res.rows[0].winery_name + " winery",
+                "techSheet" : res.rows[0].source_file,
+                "properties": {
+                    "ABV" : res.rows[0].pct_alcohol,
+                    "Tannins" : res.rows[0].ta,
+                    "pH" : res.rows[0].ph
+                }
+            });
+        }
+    });
+}
+
+exports.getUserFavoriteInfo = (id, client, callback) => {
+    client.query("SELECT users.username FROM users WHERE users.user_id = $1;",
+        [id],
+        (err, res) => {
+        if(err) console.log(err);
+        client.query("SELECT qualities.quality_name FROM qualities WHERE qualities.quality_id = (SELECT favorite_qualities.quality_id FROM favorite_qualities WHERE favorite_qualities.user_id = $1);",
+            [id],
+            (err2, res2) => {
+                if(err2) console.log(err2);
+                client.query("SELECT wineries.winery_name FROM wineries WHERE wineries.winery_id = (SELECT favorite_wineries.winery_id FROM favorite_wineries WHERE favorite_wineries.user_id = $1);",
+                    [id],
+                    (err3, res3) => {
+                        if(err3) console.log(err3);
+                        client.query("SELECT techsheets.source_file FROM techsheets WHERE techsheets.techsheet_id = (SELECT favorite_techsheets.techsheet_id FROM favorite_techsheets WHERE favorite_techsheets.user_id = $1);",
+                            [id],
+                            (err4, res4) => {
+                                if(err4) console.log(err4);
+                                callback({
+                                    "username": res.rows[0].username,
+                                    "qualities": res2.rows,
+                                    "wineries":  res3.rows,
+                                    "techsheets": res4.rows
+                                });
+                            }
+                        );
+                    }
+                );
+            }
+        );
+    });
+}
+
+exports.getUsername = (id, client, callback) => {
+    client.query("SELECT username FROM users WHERE user_id = $1;",
+        [id],
+        (err, res) => {
+            if(err) {
+                console.log(err);
+                callback(null);
+            } else {
+                callback(res.rows[0].username);
+            }
+        }
+    );
+}
+
+exports.logWineView = (userId, bottleId, ip, client, callback) => {
+    client.query("INSERT INTO wine_views (user_id, bottle_id, viewer_ip) VALUES ($1, $2, $3);",
+    [userId, bottleId, ip],
+    (err, res) => {
+        if(err) console.log(err);
+        callback();
+    });
+}
+
+exports.randomWineId = (client, callback) => {
+    client.query("SELECT bottle_id FROM bottle_data ORDER BY RANDOM() LIMIT 1;",
+    (err, res) => {
+        if(err) {
+            console.log(err);
+            callback(null);
+        } else {
+            callback(res.rows[0].bottle_id);
+        }
+    });
+}
+
+exports.addFavoriteWinery = (userId, wineryId, client, callback) => {
+    client.query("INSERT INTO favorite_wineries (user_id, winery_id) VALUES ($1, $2);",
+        [userId, wineryId],
+        (err, res) => {
+            if(err) console.log(err);
+            callback();
+        });
+}
+
+exports.delFavoriteWinery = (userId, wineryId, client, callback) => {
+    client.query("DELETE FROM favorite_wineries WHERE user_id = $1 AND winery_id = $2;",
+        [userId, wineryId],
+        (err, res) => {
+            if(err) console.log(err);
+            callback();
+        });
+}
+
+exports.addFavoriteAva = (userId, avaId, client, callback) => {
+    client.query("INSERT INTO favorite_ava (user_id, ava_id) VALUES ($1, $2);",
+        [userId, avaId],
+        (err, res) => {
+            if(err) console.log(err);
+            callback();
+        });
+}
+
+exports.delFavoriteAva = (userId, avaId, client, callback) => {
+    client.query("DELETE FROM favorite_ava WHERE user_id = $1 AND ava_id = $2;",
+        [userId, avaId],
+        (err, res) => {
+            if(err) console.log(err);
+            callback();
+        });
+}
+
+exports.addFavoriteQuality = (userId, qualityId, client, callback) => {
+    client.query("INSERT INTO favorite_qualities (user_id, quality_id) VALUES ($1, $2);",
+        [userId, qualityId],
+        (err, res) => {
+            if(err) console.log(err);
+            callback();
+        });
+}
+
+exports.delFavoriteQuality = (userId, qualityId, client, callback) => {
+    client.query("DELETE FROM favorite_qualities WHERE user_id = $1 AND quality_id = $2;",
+        [userId, qualityId],
+        (err, res) => {
+            if(err) console.log(err);
+            callback();
+        });
+}
+
+exports.addFavoriteSheet = (userId, sheetId, client, callback) => {
+    client.query("INSERT INTO favorite_techsheets (user_id, techsheet_id) VALUES ($1, $2);",
+        [userId, sheetId],
+        (err, res) => {
+            if(err) console.log(err);
+            callback();
+        });
+}
+
+exports.delFavoriteSheet = (userId, sheetId, client, callback) => {
+    client.query("DELETE FROM favorite_techsheets WHERE user_id = $1 AND techsheet_id = $2;",
+        [userId, sheetId],
+        (err, res) => {
+            if(err) console.log(err);
+            callback();
+        });
+}
---
+++ server/main.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const session = require('express-session');
+const queryHelper = require('./js/queryHelper.js');
+const bcrypt = require("bcrypt");
+const flash = require('express-flash');
+const passport = require('passport');
+
+const app = express();
+const port = process.env.PORT || 8080;
+const api = express.Router();
+
+const { Pool } = require("pg");
+
+const popularityJS = require('./js/popularity.js');
+
+const secrets = require('./js/secrets.js');
+secrets.getSecret('DB_USER').then(user => { secrets.getSecret('DB_PASS').then(pass => {
+
+    const conn = new Pool({
+        host: process.env.CONN_PORT || '10.115.240.3',
+        port: 5432,
+        database: "postgres",
+        user: user,
+        password: pass
+    });
+    
+    conn.connect().then(() => {
+
+        console.log('DB connected.');
+
+        require("./js/api.js")(api, conn, popularityJS);
+    
+        const initializePassport = require("./js/passportConfig");
+        initializePassport(passport, conn, bcrypt);
+        
+        app.set('view engine', 'handlebars');
+        
+        app.engine('handlebars', handlebars.engine({
+            layoutsDir: './views/layouts/'
+        }));
+        
+        app.use(express.urlencoded({ extended: false }));
+        app.use(express.static('public'));
+        app.use(session({
+            secret: "akjfhakjaw8723bs873ka7ykhasi7yq2kjhfa8k72ydbk37",
+            saveUninitialized: true,
+            resave: false 
+        }));
+        
+        app.use(passport.initialize());
+        app.use(passport.session());
+        app.use(flash());
+        
+        app.use("/api", api);
+        
+        app.use((req, res, next) => {
+            res.locals.pack = {
+                template: null,
+                config: {
+                    layout: 'main',
+                    css: null,
+                    js: null,
+                    data: null,
+                    user: req.session.passport ? req.session.passport.user.username : null
+                }
+            };
+            next();
+        });
+        
+        require('./js/routes.js')(app, conn, queryHelper, passport, bcrypt, flash, popularityJS);
+        
+        app.use((req, res) => {
+            if(res.locals.pack.template) res.render(res.locals.pack.template, res.locals.pack.config);
+        });
+        
+        app.listen(port, () => console.log(`App open at http://localhost:${port}/`));
+
+    });
+
+});});
